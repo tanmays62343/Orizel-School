65f27fa955e07e758b23759728cec95233fad624
tanmays62343 <tanmays62343@gmail.com>
2023-10-13 18:45:27 +0530

    added bottom Navigation bar

--- app/src/main/java/com/orizel/activities/MainActivity.kt
+++ app/src/main/java/com/orizel/activities/MainActivity.kt
@@ -14,11 +14,15 @@ import com.orizel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding : ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
+
     //for top action bar setup
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     //for firestore initialization
     private lateinit var firestore: FirebaseFirestore
+
+    //for firebase authentication
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +35,40 @@ class MainActivity : AppCompatActivity() {
 
         binding?.logout?.setOnClickListener {
             firebaseAuth.signOut()
-            Toast.makeText(this, "Logged Out",
-                Toast.LENGTH_LONG).show()
-            intent = Intent(this,LoginActivity::class.java)
+            Toast.makeText(
+                this, "Logged Out",
+                Toast.LENGTH_LONG
+            ).show()
+            intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        binding?.btNavigationBar?.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.btNavigation_home -> {
+                    Toast.makeText(
+                        this, "Hello",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+                R.id.btNavigation_Cart -> {
+                    Toast.makeText(this, "Cart",
+                        Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.btNavigation_orders -> {
+                    Toast.makeText(this, "Orders",
+                        Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                    true
+                }
+            }
+        }
+
 
     }
 
@@ -57,14 +90,16 @@ class MainActivity : AppCompatActivity() {
     //we are telling here that we have our own action bar
     private fun setupDrawerLayout() {
         setSupportActionBar(binding?.topAppBar)
-        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding?.drawerLayout,
-            R.string.app_name,R.string.app_name)
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, binding?.drawerLayout,
+            R.string.app_name, R.string.app_name
+        )
         actionBarDrawerToggle.syncState()
     }
 
     //Here we are telling that we have handled the top bar actions
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
