873331ccd479140af65b6af68d27f860b9c8bc4e
Aman1953 <mintuaman111@gmail.com>
2023-11-06 11:54:45 +0530

    Added Contact US UI

---
+++ app/src/main/java/com/orizel/activities/About_us.kt
@@ -0,0 +1,12 @@
+package com.orizel.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.orizel.R
+
+class About_us : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
