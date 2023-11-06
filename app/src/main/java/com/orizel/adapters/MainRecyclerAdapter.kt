21e30fe7d565110ab662440428d745a571449d51
tanmays62343 <tanmays62343@gmail.com>
2023-11-06 11:53:38 +0530

    Handled Recycler buttons

--- app/src/main/java/com/orizel/adapters/MainRecyclerAdapter.kt
+++ app/src/main/java/com/orizel/adapters/MainRecyclerAdapter.kt
@@ -1,22 +1,32 @@
 package com.orizel.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.orizel.R
+import com.orizel.activities.CartActivity
 import com.orizel.models.FoodProduct
 import com.squareup.picasso.Picasso
 
 
-class MainRecyclerAdapter (val requiredContext : Context, private val foodProducts: MutableList<FoodProduct>) :
+class MainRecyclerAdapter(
+    val context: Context,
+    private val foodProducts: MutableList<FoodProduct>
+) :
     RecyclerView.Adapter<MainRecyclerAdapter.MyViewHolder>() {
 
+    private var quantities: Int = 0
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
         return MyViewHolder(view)
     }
 
@@ -29,13 +39,39 @@ class MainRecyclerAdapter (val requiredContext : Context, private val foodProduc
         holder.itemPrice.text = foodProducts[position].price.toString()
         //Inserting image
         Picasso.get().load(foodProducts[position].imageUri).into(holder.image)
+
+        holder.addItem.setOnClickListener {
+            quantities++
+            holder.quantity.text = quantities.toString()
+        }
+
+        holder.subtractItem.setOnClickListener {
+            if (quantities > 0) {
+                quantities--
+                holder.quantity.text = quantities.toString()
+            } else {
+                return@setOnClickListener
+            }
+        }
+
+        holder.addToCart.setOnClickListener {
+            quantities = 0
+            holder.quantity.text = quantities.toString()
+            Toast.makeText(context, "Proceed To cart", Toast.LENGTH_SHORT).show()
+            val intent = Intent(context,CartActivity::class.java)
+            intent.putExtra("QUANTITY",quantities)
+        }
+
     }
 
-    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        val itemName : TextView = itemView.findViewById(R.id.food_Product_Name)
-        val itemPrice : TextView = itemView.findViewById(R.id.food_Product_price)
-        val image :ImageView = itemView.findViewById(R.id.food_product_image)
-
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemName: TextView = itemView.findViewById(R.id.food_Product_Name)
+        val itemPrice: TextView = itemView.findViewById(R.id.food_Product_price)
+        val image: ImageView = itemView.findViewById(R.id.food_product_image)
+        val addItem: Button = itemView.findViewById(R.id.add)
+        val subtractItem: Button = itemView.findViewById(R.id.subtract)
+        val quantity: TextView = itemView.findViewById(R.id.quantity)
+        val addToCart : Button = itemView.findViewById(R.id.addToCart)
     }
 
 }
