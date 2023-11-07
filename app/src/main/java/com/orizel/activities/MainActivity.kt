8ba0bfbd27bb7ad900533bb5f6daa77abfb2be1d
tanmays62343 <tanmays62343@gmail.com>
2023-11-07 14:54:32 +0530

    Added FoodProductsFragment

--- app/src/main/java/com/orizel/activities/MainActivity.kt
+++ app/src/main/java/com/orizel/activities/MainActivity.kt
@@ -1,47 +1,39 @@
 package com.orizel.activities
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.orizel.R
-import com.orizel.adapters.MainRecyclerAdapter
 import com.orizel.databinding.ActivityMainBinding
-import com.orizel.models.FoodProduct
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding: ActivityMainBinding? = null
+    private lateinit var binding: ActivityMainBinding
 
     //for top action bar setup
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
-    //for firestore initialization
-    private lateinit var firestore: FirebaseFirestore
-
     //for firebase authentication
     private lateinit var firebaseAuth: FirebaseAuth
 
-    private var foodProductsList = mutableListOf<FoodProduct>()
+    //initializing the foodProducts fragments
+    private val foodProductsFragment = FoodProductsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
+        setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
 
         setupViews()   //Setting up all views Here
 
         //TODO : Shift the logout buttons place
-        binding?.logout?.setOnClickListener {
+        binding.logout.setOnClickListener {
             firebaseAuth.signOut()
             Toast.makeText(
                 this, "Logged Out",
@@ -54,11 +46,10 @@ class MainActivity : AppCompatActivity() {
 
 
         //Handling the bottom Navigation Bar listener
-        binding?.btNavigationBar?.setOnItemSelectedListener {
+        binding.btNavigationBar.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.btNavigation_home -> {
-                    Toast.makeText(this, "Hello",
-                        Toast.LENGTH_SHORT).show()
+                    setupFragment()
                     true
                 }
                 R.id.btNavigation_Cart -> {
@@ -85,38 +76,21 @@ class MainActivity : AppCompatActivity() {
     //Aggregating all the views here
     private fun setupViews() {
         setupDrawerLayout()
-        setupRecyclerView()
-        setupFireStore()
+        setupFragment()
     }
 
-    //Setting up the firebase Database
-    @SuppressLint("NotifyDataSetChanged")
-    private fun setupFireStore() {
-        firestore = FirebaseFirestore.getInstance()
-        val collectionReference = firestore.collection("foodProduct")
-        collectionReference.addSnapshotListener { value, error ->
-            if(value == null || error != null){
-                Toast.makeText(this, "Cannot fetch data",
-                    Toast.LENGTH_SHORT).show()
-                return@addSnapshotListener
-            }
-            foodProductsList.clear()
-            foodProductsList.addAll(value.toObjects(FoodProduct::class.java))
-            binding?.mainRecyclerView?.adapter?.notifyDataSetChanged()
+    //setting up the initial fragment
+    private fun setupFragment(){
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.fragmentContainer.id,foodProductsFragment)
+            commit()
         }
-    }
-
-    //Recycler view setup
-    private fun setupRecyclerView() {
-        // data items
-        binding!!.mainRecyclerView.adapter = MainRecyclerAdapter(this,foodProductsList)
-        binding?.mainRecyclerView?.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
     }
 
     //we are telling here that we have our own action bar
     private fun setupDrawerLayout() {
-        setSupportActionBar(binding?.topAppBar)
-        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding?.drawerLayout,
+        setSupportActionBar(binding.topAppBar)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerLayout,
             R.string.app_name,R.string.app_name)
         actionBarDrawerToggle.syncState()
     }
